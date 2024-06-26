5767d0b527a478b63de301143936e7427ca9bb9a
Omar Ramirez <omardramirez2002@gmail.com>
2024-06-26 14:30:36 -0600

    Removes excess code after testing util module

--- src/states/JoinLobbyState.lua
+++ src/states/JoinLobbyState.lua
@@ -56,49 +56,6 @@ function JoinLobbyState:requestLobbies(self,udp)
 
 end
 
-function JoinLobbyState:parseLobbyData(data, command)
-    local lobbyId = JoinLobbyState:parseLobbyId(data)
-    if command == "addNewLobby" or command == "initLobbies" then
-        local playersTable = JoinLobbyState:parsePlayerInfo(data)
-        return tonumber(lobbyId), playersTable
-    elseif command == "addLobbyState" or command == "initLobbyStates" then
-        local statesTable = JoinLobbyState:parseLobbyInfo(data)
-        return tonumber(lobbyId), statesTable
-    end
-
-end
-
-function JoinLobbyState:parseLobbyId(string)
-    print(string)
-    local id_pattern = "lobby:(%d+)"
-    return string:match(id_pattern)
-end
-
-function JoinLobbyState:parsePlayerInfo(string)
-    print ("this is string in paseplayer"..string)
-    local players = {}
-    local player_pattern1 = "{%s*(%w+)%s*,%s*(%d+)%s*}" --first pattern to just for words
-    local player_pattern2 = "{%s*([^,]+)%s*,%s*(%d+)%s*}" --second pattern to account for colons
-    for address, port in string:gmatch(player_pattern2) do
-        print("parseplayer address:"..address)
-        print("parseplayer port:"..port)
-        table.insert(players, {peerAddress = address, peerPort = tonumber(port)})
-    end
-    return players
-end
-
-function JoinLobbyState:parseLobbyInfo(string)
-    print ("this is string in parse Lobby Info"..string)
-    local lobbyState = {}
-    local player_pattern2 = "{([^,]+),(%d+),(%d+)}" --second pattern to account for colons
-    for state, playerCount, limit  in string:gmatch(player_pattern2) do
-        print("parse lobby state:"..state)
-        print("parse lobby player count:"..playerCount)
-        print("parse lobby player count:"..limit)
-        table.insert(lobbyState, {state = state, playerCount = tonumber(playerCount), limit = tonumber(limit)})
-    end
-    return lobbyState
-end
 
 
 function JoinLobbyState:update(dt)
